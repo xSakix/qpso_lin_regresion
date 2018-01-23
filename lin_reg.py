cae2f4aa92c77531d8133c4aca0b3c1bbd9b24dd
xSakix <seckarovci@gmail.com>
2018-01-23 16:17:31 +0100

    sklearn is best in estimating lin. regression. How are they doing it??? They write in their doc that they use Ordinary Least Squares, e.g. the general matrix solving method, but the results vs such method are different...hm

--- lin_reg.py
+++ lin_reg.py
@@ -1,3 +1,7 @@
+#
+# inspired by https://dsgazette.com/2018/01/10/linear-regression-by-hand/
+#
+
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn import datasets
@@ -6,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 class QuantumParticle:
-    def __init__(self, m=2):
+    def __init__(self,m=2):
         self.M = m
         self.w = np.random.uniform(-1., 1., self.M)
         self.p_w = np.array(self.w)
@@ -28,7 +32,7 @@ class QuantumParticle:
 
     # lin regresion yi = b0+b1*xi1+b2*xi2+...+bn*xin
     def evaluate(self, x):
-        #b0 other
+        # b0
         # x = np.array(x)
         # i, j = np.shape(x)
         # x = np.resize(x,(i,j+1))
@@ -51,8 +55,10 @@ class QPSOLinearRegressor:
         self.iterations = iterations
         self.population = []
         self.fitness_history = []
+
         for i in range(self.population_size):
             self.population.append(QuantumParticle(self.M))
+
 
     def run(self, data, t):
 
@@ -77,15 +83,30 @@ class QPSOLinearRegressor:
         return self.population[0]
 
 
+def compute_beta(X, y):
+    one = np.matmul(np.transpose(X), X)
+    one = np.linalg.inv(one)
+
+    return np.matmul(np.matmul(one, X.T), y)
+
+
+def predict(b, X):
+    return X.dot(b)
+
+
 def test():
     boston = datasets.load_boston()
     y = boston.target
     # print(len(boston.data[0]))
     # print(boston.data[0])
     # print(boston.target[0])
+    b = compute_beta(boston.data, y)
+    pred = predict(b, boston.data)
+
     lin_reg = QPSOLinearRegressor(1000, 300, len(boston.data[0]))
     best = lin_reg.run(boston.data, y)
     plt.plot(lin_reg.fitness_history)
+    plt.title('qpso fitness evolution')
     plt.show()
 
     y = np.reshape(y, (506, 1))
@@ -95,16 +116,22 @@ def test():
     qpso_error = mean_squared_error(qpso_pred, y)
     lr_pred = lr.predict(boston.data)
     lr_error = mean_squared_error(lr_pred, y)
+    matrix_error = mean_squared_error(pred, y)
 
     print('qpso error:' + str(qpso_error))
     print('sklearn lr error:' + str(lr_error))
+    print('matrix computation error:' + str(matrix_error))
 
     # print(lr.coef_)
     # print(best.w)
 
-    plt.plot(boston.target, 'o')
-    plt.plot(qpso_pred, 'r')
+    plt.plot(boston.target, 'o', color='green')
     plt.plot(lr_pred, 'b')
+    plt.plot(qpso_pred, 'r')
+    plt.plot(pred, 'yellow')
+    plt.title('Comparison of lin. regression computation')
+    plt.legend(['boston data set', 'qpso lin. regresion prediction', 'sklearn lin. regresion prediction',
+                'matrix computed lin. regresion prediction'])
     plt.show()
 
 
